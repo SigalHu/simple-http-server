9071e001e5478f1ae0e024c92853d81dcbae2dd4
sigalhu <sigalhu@foxmail.com>
2019-06-13 21:44:12 +0800

    update simple http server

---
+++ http_server_start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+HOME=$(dirname $(readlink -f $0))
+PORT=$1
+
+PYTHON_VERSION=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
+
+if [[ ${PYTHON_VERSION} == 3 ]]; then
+    2to3 -w ${HOME}/http_server_start.py
+fi
+nohup python ${HOME}/http_server_start.py ${PORT} 1>${HOME}/nohup.out 2>&1 &
